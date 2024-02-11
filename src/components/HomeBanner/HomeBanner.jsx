d1793aac5d2d898871d2b555dc626f56c6aebb08
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-11 09:27:33 +0600

    conditionally add class name

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -5,56 +5,35 @@ import { useSelector } from "react-redux";
 const HomeBanner = ({ blog }) => {
   const { loggedInUser } = useSelector((state) => state.auth);
   const { title, tags, image, createdAt, creatorFullName, creatorImage } = blog;
+
+  const wrapperClassName = loggedInUser
+    ? "banner__auth-info-wrapper"
+    : "banner__info-wrapper";
+
   return (
     <div className="banner">
       <img src={image} className="banner__image" alt="Blog Banner" />
-      {loggedInUser ? (
-        <div className="banner__auth-info-wrapper">
-          <div className="banner__auth-info-wrapper__heading">
-            <div className="banner__auth-info-wrapper__heading--category-badge">
-              {tags}
-            </div>
-            <h3 className="banner__auth-info-wrapper__heading--title">
-              {title}
-            </h3>
+      <div className={wrapperClassName}>
+        <div className={`${wrapperClassName}-heading`}>
+          <div className={`${wrapperClassName}-heading-category-badge`}>
+            {tags}
           </div>
-          <div className="banner__auth-info-wrapper__identity">
-            <img
-              src={creatorImage}
-              alt="Author"
-              className="banner__auth-info-wrapper__identity--image"
-            />
-            <p className="banner__auth-info-wrapper__identity--name">
-              {creatorFullName}
-            </p>
-            <p className="banner__auth-info-wrapper__identity--createdAt">
-              {createdAt}
-            </p>
-          </div>
+          <h3 className={`${wrapperClassName}-heading-title`}>{title}</h3>
         </div>
-      ) : (
-        <div className="banner__info-wrapper">
-          <div className="banner__info-wrapper__heading">
-            <div className="banner__info-wrapper__heading--category-badge">
-              {tags}
-            </div>
-            <h3 className="banner__info-wrapper__heading--title">{title}</h3>
-          </div>
-          <div className="banner__info-wrapper__identity">
-            <img
-              src={creatorImage}
-              alt="Author"
-              className="banner__info-wrapper__identity--image"
-            />
-            <p className="banner__info-wrapper__identity--name">
-              {creatorFullName}
-            </p>
-            <p className="banner__info-wrapper__identity--createdAt">
-              {createdAt}
-            </p>
-          </div>
+        <div className={`${wrapperClassName}-identity`}>
+          <img
+            src={creatorImage}
+            alt="Author"
+            className={`${wrapperClassName}-identity-image`}
+          />
+          <p className={`${wrapperClassName}-identity-name`}>
+            {creatorFullName}
+          </p>
+          <p className={`${wrapperClassName}-identity-created-at`}>
+            {createdAt}
+          </p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
