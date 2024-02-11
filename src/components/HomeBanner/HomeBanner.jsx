0431f5295f28f9dd1139c9c5f1871f0794443cd9
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-11 10:45:44 +0600

    banner linking

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "@components/HomeBanner/homeBanner.scss";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const HomeBanner = ({ blog }) => {
   const { loggedInUser } = useSelector((state) => state.auth);
-  const { title, tags, image, createdAt, creatorFullName, creatorImage } = blog;
+  const { id, title, tags, image, createdAt, creatorFullName, creatorImage } =
+    blog;
 
   const wrapperClassName = loggedInUser
     ? "banner__auth-info-wrapper"
@@ -12,28 +14,30 @@ const HomeBanner = ({ blog }) => {
 
   return (
     <div className="banner">
-      <img src={image} className="banner__image" alt="Blog Banner" />
-      <div className={wrapperClassName}>
-        <div className={`${wrapperClassName}-heading`}>
-          <div className={`${wrapperClassName}-heading-category-badge`}>
-            {tags}
+      <Link to={`/blog/${id}`}>
+        <img src={image} className="banner__image" alt="Blog Banner" />
+        <div className={wrapperClassName}>
+          <div className={`${wrapperClassName}-heading`}>
+            <div className={`${wrapperClassName}-heading-category-badge`}>
+              {tags}
+            </div>
+            <h3 className={`${wrapperClassName}-heading-title`}>{title}</h3>
           </div>
-          <h3 className={`${wrapperClassName}-heading-title`}>{title}</h3>
+          <div className={`${wrapperClassName}-identity`}>
+            <img
+              src={creatorImage}
+              alt="Author"
+              className={`${wrapperClassName}-identity-image`}
+            />
+            <p className={`${wrapperClassName}-identity-name`}>
+              {creatorFullName}
+            </p>
+            <p className={`${wrapperClassName}-identity-created-at`}>
+              {createdAt}
+            </p>
+          </div>
         </div>
-        <div className={`${wrapperClassName}-identity`}>
-          <img
-            src={creatorImage}
-            alt="Author"
-            className={`${wrapperClassName}-identity-image`}
-          />
-          <p className={`${wrapperClassName}-identity-name`}>
-            {creatorFullName}
-          </p>
-          <p className={`${wrapperClassName}-identity-created-at`}>
-            {createdAt}
-          </p>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
