0b75feac3ce274b9fb093099116710345c3a4a8b
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-12 09:37:07 +0600

    add propTypes

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "@components/HomeBanner/homeBanner.scss";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -6,7 +7,7 @@ import { Link } from "react-router-dom";
 const HomeBanner = ({ blog }) => {
   const { loggedInUser } = useSelector((state) => state.auth);
   const { id, title, tags, image, createdAt, creatorFullName, creatorImage } =
-    blog;
+    blog || {};
 
   const wrapperClassName = loggedInUser
     ? "banner__auth-info-wrapper"
@@ -40,6 +41,18 @@ const HomeBanner = ({ blog }) => {
       </Link>
     </div>
   );
+};
+
+HomeBanner.propTypes = {
+  blog: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    tags: PropTypes.array.isRequired,
+    image: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    creatorFullName: PropTypes.string.isRequired,
+    creatorImage: PropTypes.string.isRequired,
+  }),
 };
 
 export default HomeBanner;
