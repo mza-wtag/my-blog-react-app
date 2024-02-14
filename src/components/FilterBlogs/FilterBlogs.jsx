30919ad0ab65cfa85aeafbeffafc4a36a886d301
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-14 14:14:50 +0600

    issue resolved

--- src/components/FilterBlogs/FilterBlogs.jsx
+++ src/components/FilterBlogs/FilterBlogs.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import tags from "@constants/tags.json";
 import { filterByTag } from "@actions/blogActions";
 import "@components/FilterBlogs/filterBlogs.scss";
 
 const FilterBlogs = () => {
   const dispatch = useDispatch();
+  const selectedTag = useSelector((state) => state.filter.filteredTag);
+  const [currentTag, setCurrentTag] = useState(selectedTag);
+
+  useEffect(() => {
+    setCurrentTag(selectedTag);
+  }, [selectedTag]);
+
   const handleChange = (event) => {
-    dispatch(filterByTag(event.target.value));
+    const tag = event.target.value;
+    setCurrentTag(tag);
+    dispatch(filterByTag(tag));
   };
 
   return (
     <div className="filter-container">
-      <label htmlFor="filter">Filter By:</label>
-      <select id="filter" onChange={handleChange}>
-        <option value="">Select an option</option>
+      <label htmlFor="filter">Filter By Tag Names:</label>
+      <select id="filter" value={currentTag} onChange={handleChange}>
+        <option value="">All</option>
         {tags &&
           tags.map((option, index) => (
             <option key={index} value={option.value}>
