a6d5311098d75fc27c69663d3bdc851e61063670
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-15 12:33:35 +0600

    pagination functionality added

--- src/components/BlogList/BlogList.jsx
+++ src/components/BlogList/BlogList.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import BlogCard from "@components/BlogCard/BlogCard";
 import NotFound from "@components/NotFound/NotFound";
+import Button from "@components/Button/Button";
 import "@components/BlogList/blogList.scss";
 
 const BlogList = ({ blogs }) => {
   const searchQuery = useSelector((state) => state.search.searchQuery);
   const filteredTag = useSelector((state) => state.filter.filteredTag);
+  const [blogsToShow, setBlogsToShow] = useState(9);
 
   const filteredBlogs = blogs.filter((blog) => {
     const titleMatches = blog.title
@@ -16,14 +18,27 @@ const BlogList = ({ blogs }) => {
     return titleMatches && tagMatches;
   });
 
+  const handleShowMore = () => {
+    setBlogsToShow((prevCount) => prevCount + 6);
+  };
+
   return (
-    <div className="blog-list">
-      {filteredBlogs.length > 0 ? (
-        filteredBlogs.map((blog) => <BlogCard key={blog.id} blog={blog} />)
-      ) : (
-        <NotFound text="No Blogs Found." />
+    <>
+      <div className="blog-list">
+        {filteredBlogs.length > 0 ? (
+          filteredBlogs
+            .slice(0, blogsToShow)
+            .map((blog) => <BlogCard key={blog.id} blog={blog} />)
+        ) : (
+          <NotFound text="No Blogs Found." />
+        )}
+      </div>
+      {filteredBlogs.length > blogsToShow && (
+        <Button className="blog-list__button" onClick={handleShowMore}>
+          Load More
+        </Button>
       )}
-    </div>
+    </>
   );
 };
 
