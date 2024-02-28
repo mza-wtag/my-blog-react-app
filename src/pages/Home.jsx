2d5271b830042f6266deb6198ddf3cce28f72d75
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-28 14:56:07 +0600

    if no data then it shows error ,fixed it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import BlogList from "@components/BlogList/BlogList";
+import NotFound from "@components/NotFound/NotFound";
 import HomeBanner from "@components/HomeBanner/HomeBanner";
 import FilterBlogs from "@components/FilterBlogs/FilterBlogs";
+import BlogList from "@components/BlogList/BlogList";
 import tags from "@constants/tags.json";
 
 const Home = () => {
   const blogs = useSelector((state) => state.blog);
+
   return (
     <div className="container">
-      <HomeBanner blog={blogs[0]} />
-      <div className="common-title-filter-holder">
-        <h2>Latest Posts</h2>
-        <FilterBlogs tags={tags} />
-      </div>
-      <BlogList blogs={blogs} />
+      {blogs.length === 0 ? (
+        <NotFound text={"No Data Found"} />
+      ) : (
+        <>
+          <HomeBanner blog={blogs[0]} />
+          <div className="common-title-filter-holder">
+            <h2>Latest Posts</h2>
+            <FilterBlogs tags={tags} />
+          </div>
+          <BlogList blogs={blogs} />
+        </>
+      )}
     </div>
   );
 };
