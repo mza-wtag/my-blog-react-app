bce8155051c2e53ff83bf5727f9f405a3baaf22c
mza-wtag <mahfuz.zaman@welldev.io>
2024-03-07 13:42:38 +0600

    handle search component on login and register page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { searchBlogPosts } from "@actions/blogActions";
 import SearchIcon from "@assets/images/icons/search.svg";
 import "@components/Search/search.scss";
@@ -7,11 +8,20 @@ import "@components/Search/search.scss";
 const Search = () => {
   const dispatch = useDispatch();
   const query = useSelector((state) => state?.search?.searchQuery);
+  const location = useLocation();
 
   const handleSearch = (event) => {
     const searchQuery = event.target.value;
     dispatch(searchBlogPosts(searchQuery));
   };
+
+  const shouldRenderSearch = !["/login", "/register"].includes(
+    location.pathname
+  );
+
+  if (!shouldRenderSearch) {
+    return null;
+  }
 
   return (
     <div className="navbar__search">
